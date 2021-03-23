1be6130cd27664013dbc5058036ed9279d10b9ac
Rebecca Walton <yshu637@aucklanduni.ac.nz>
2021-03-23 21:41:49 +1300

    passengers

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+ <Passengers />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Passengers from './components/Passengers.vue'
 
 export default {
-  name: 'App',
+  name: 'app',
   components: {
-    HelloWorld
+    Passengers
   }
 }
 </script>
